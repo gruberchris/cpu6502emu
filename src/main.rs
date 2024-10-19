d54bb38dbcbc510c6550307e8488a78a9a08fc75
Chris Gruber <gruber.christopher@gmail.com>
2024-10-19 16:03:58 -0500

    try executing two inline programs

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,29 @@ mod cpu;
 mod memory;
 mod types;
 
-use cpu::{Cpu, INS_LDA_IM};
+use cpu::{Cpu, INS_LDA_IM, INS_LDA_ZP};
 use memory::Memory;
 
 fn main() {
     let mut cpu = Cpu::new();
     let mut memory = Memory::new();
-    let mut cycles = 2;
 
+    // initialize the memory
     cpu.reset(&mut memory);
+    // initialize the clock cycles
+    let mut cycles = 2;
 
     // example inline virtual machine code program to test with
     memory.write(0xFFFC, INS_LDA_IM);
     memory.write(0xFFFD, 0x42);
+    cpu.execute(&mut cycles, &mut memory);
 
+    // initialize and run a second inline virtual machine code program
+    cpu.reset(&mut memory);
+    cycles = 3;
+
+    memory.write(0xFFFC, INS_LDA_ZP);
+    memory.write(0xFFFD, 0x42);
+    memory.write(0x0042, 0x84);
     cpu.execute(&mut cycles, &mut memory);
 }
